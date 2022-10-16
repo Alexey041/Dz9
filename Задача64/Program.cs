87f0631b9fdb72b59efdd9fc14c9f83bda2e50b0
Alex <bazz3k@ya.ru>
2022-10-16 15:48:16 +0300

    Добавил все

---
+++ Задача64/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задайте значение N. Напишите программу, которая выведет все натуральные 
+числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+Console.Clear();
+Console.Write("Введите число ");
+int a = int.Parse(Console.ReadLine()!);
+
+Integers(a);
+
+void Integers (int a)
+{
+    if (a == 0) return;
+    Console.Write(a);
+    Integers(a - 1);
+}
---
+++ Задача66/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных
+элементов в промежутке от M до N. Рекурсивно
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+Console.Clear();
+Console.Write("Введите число M ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Sum(m) - 1);
+
+
+int Sum (int m)
+{
+    if (m <= n)
+    {
+        return m + Sum(m + 1);
+    }
+    else
+    {
+        return 1;
+    }
+}
